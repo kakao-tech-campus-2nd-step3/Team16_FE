9aca69f57d2f4bc9a570451685d36468c46a5610
HyeonChae1104 <211063@jnu.ac.kr>
2024-09-20 22:03:48 +0900

    chore: 경로 오류 수정
    styles의 경로 오류가 있어 상대경로로 수정하였습니다.
    
    #7

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Container } from './components/common/layouts/Container'; // Container 컴포넌트 import
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Container
+          maxWidth="1200px"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
         >
-          Learn React
-        </a>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Container>
       </header>
     </div>
   );
