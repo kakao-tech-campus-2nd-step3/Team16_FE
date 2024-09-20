e84aea6496d24789fd6982556f61459929432ce4
HyeonChae1104 <211063@jnu.ac.kr>
2024-09-20 23:01:29 +0900

    chore: App.css, index.css 제거 및 스타일 관리 구조 개선
    - App.css, index.css 파일 삭제
    - src/index.tsx에 import '@/styles' 추가
    - 현재 craco 이슈로 인해 import './styles/index'로 대체
    
    #4

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import '@styles/index'
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -17,3 +19,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export {};
