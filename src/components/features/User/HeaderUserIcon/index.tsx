b5017f80d8b880fa0c19816fe4a4bdd62608eae8
박건규 <geongyu09@gmail.com>
2024-09-26 20:41:53 +0900

    chore(HeaderUserIcon.tsx): 로딩중 혹은 요청 에러시 기본 유저 이미지가 보이도록 수정
    #22

--- src/components/features/User/HeaderUserIcon/index.tsx
+++ src/components/features/User/HeaderUserIcon/index.tsx
@@ -1,18 +1,15 @@
 import { useGetUserProfile } from '@/api/hooks/useGetUser';
 import UserProfile from '@/components/common/User/UserProfile';
 
+const DEFAULT_PROFILE_IMAGE = '/images/default_profile_image.png';
+
 export default function HeaderUserIcon() {
   const { data: profileImage, status } = useGetUserProfile();
 
-  if (status === 'pending') {
-    return <div>Loading...</div>;
-  }
-
-  if (status === 'error') {
-    return <div>Error...</div>;
-  }
-
-  const { profile_image_url: profile } = profileImage;
+  const profile =
+    status === 'pending' || status === 'error'
+      ? DEFAULT_PROFILE_IMAGE
+      : profileImage.profile_image_url;
 
   return <UserProfile profileImage={profile} />;
 }
