1b790db98c84ae0eeb5d3c14bebf931ac167b9d3
박건규 <geongyu09@gmail.com>
2024-09-27 16:01:06 +0900

    refactor(Logo.tsx): 불필요한 height props 제거
    #22

--- src/components/common/Icons/Logo/index.tsx
+++ src/components/common/Icons/Logo/index.tsx
@@ -3,8 +3,8 @@ import type { HTMLAttributes } from 'react';
 
 type Props = {
   width?: string;
-  height?: string;
 } & HTMLAttributes<HTMLImageElement>;
+
 export const Logo: React.FC<Props> = ({ ...rest }) => {
   return <StyledLogo src="/icons/bobting-logo.svg" alt="Logo" {...rest} />;
 };
