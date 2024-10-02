1466cff9a3aa2ae86a82fa77ff9544349d21274d
박건규 <geongyu09@gmail.com>
2024-10-02 18:18:21 +0900

    feat(Calendar): 요구사항의 변경에 따라 불필요한 코드 제거
    수정, 삭제가 제거됨에 따라서 불필요한 로직 제거

--- src/service/Calendar/components/DefaultCalendar/index.tsx
+++ src/service/Calendar/components/DefaultCalendar/index.tsx
@@ -1,5 +1,4 @@
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 
@@ -8,46 +7,15 @@ import type { Event } from '@/service/Calendar/types';
 type Props = {
   height?: number;
   evnets?: Event[];
-  //TODO: 캘린더 수정 부분 추가시 사용
-  // onChange?: () => void;
 };
 
 export const DefaultCalendar: React.FC<Props> = ({ height, evnets }) => {
   return (
     <FullCalendar
-      plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+      plugins={[dayGridPlugin, timeGridPlugin]}
       initialView="timeGridWeek"
       events={evnets}
       height={height}
-      // TODO: 캘린더 수정 추가시 사용
-      // eventDrop={(info) => {
-      //   alert(info.event.title + ' was dropped on ' + info.event.start);
-      // }}
-      // eventResizableFromStart
-      // eventDragStart={(info) => {
-      //   alert('Event ' + info.event.title + ' is being dragged');
-      // }}
-      // dragScroll
-      // droppable
-      // expandRows
-
-      // TODO: 일정 추가 혹은 일정 표시시 사용
-      // select={(info) => {
-      //   const input = prompt('Enter new title:', info.startStr);
-      //   if (input) {
-      //     console.log(info);
-      //     info.view.calendar.addEvent({
-      //       title: input,
-      //       start: info.startStr,
-      //       end: info.endStr,
-      //       allDay: info.allDay,
-      //     });
-      //   }
-      // }}
-      // selectable={true}
-      // selectMirror={true}
-      // editable={true}
-
       headerToolbar={{
         left: '',
         right: 'prev,title,next',
