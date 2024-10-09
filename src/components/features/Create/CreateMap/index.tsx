c0cb23856d78718104141703ab4b81629909b3aa
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-10-09 22:11:29 +0900

    feat(CreateMap): 카카오맵 API 활용해 표시
    - 카카오맵 API로 지도 보이기
    - geolocation 이용해 현재 위치를 기본 위치로 하기 (단, 위치 허용)
    - 지도를 클릭하여 핀 꽂고, 위도/경도 받아오기
    
     #50

--- src/components/features/Create/CreateMap/index.tsx
+++ src/components/features/Create/CreateMap/index.tsx
@@ -1,16 +1,76 @@
 import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
 
 import { breakpoints } from '@/styles/variants';
 
-export const CreateMap = () => {
-  return <MapImg />;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+const { kakao } = window;
+
+interface Coordinates {
+  lat: number;
+  lng: number;
+}
+
+export const CreateMap: React.FC = () => {
+  const [currentPosition, setCurrentPosition] = useState<Coordinates | null>(null);
+
+  const fetchCurrentPosition = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude } }) => {
+          setCurrentPosition({ lat: latitude, lng: longitude });
+        },
+        () => {
+          setCurrentPosition({ lat: 33.450701, lng: 126.570667 });
+        },
+      );
+    } else {
+      setCurrentPosition({ lat: 33.450701, lng: 126.570667 });
+    }
+  };
+
+  const initializeMap = (position: Coordinates) => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(position.lat, position.lng),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+
+    const marker = new kakao.maps.Marker({
+      position: map.getCenter(),
+      map: map,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    kakao.maps.event.addListener(map, 'click', function (mouseEvent: kakao.maps.event.MouseEvent) {
+      const latlng = mouseEvent.latLng;
+      marker.setPosition(latlng);
+    });
+  };
+
+  useEffect(() => {
+    fetchCurrentPosition();
+  }, []);
+
+  useEffect(() => {
+    if (currentPosition) {
+      initializeMap(currentPosition);
+    }
+  }, [currentPosition]);
+
+  return (
+    <>
+      <MapContainer id="map" />
+    </>
+  );
 };
 
-const MapImg = styled.div`
+const MapContainer = styled.div`
   flex: 2;
   display: flex;
-  background-color: #b4c1b9;
-
   min-height: 400px;
   width: 100%;
 
