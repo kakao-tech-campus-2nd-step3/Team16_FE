59593743feca4a4af7b04f76671efbff3dcba8e2
HyeonChae1104 <211063@jnu.ac.kr>
2024-10-10 14:30:27 +0900

    feat:(Login): 백엔드로 리다이렉트하는 카카오 로그인 기능 추가
    카카오 로그인 버튼을 통해 백엔드로 리다이렉트하는 기능을 추가하였습니다.(GET 요청)
    
    #40

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,17 @@
-export const LoginPage = () => {
-  return <div>Login Page</div>;
+import React from 'react';
+import KakaoLogin from 'src/components/common/Button/kakaoLogin';
+
+const LoginPage: React.FC = () => {
+  const handleKakaoLogin = () => {
+    window.location.href = 'http://localhost:8080/api/auth/login';
+  };
+
+  return (
+    <div>
+      <h1>로그인 페이지</h1>
+      <KakaoLogin onClick={handleKakaoLogin} />
+    </div>
+  );
 };
+
+export default LoginPage;
