f5017cfc3efc742d4413a60a59e74db8f6e5366e
박건규 <67491015+geongyu09@users.noreply.github.com>
2024-10-11 22:22:08 +0900

    [FEAT] 내 모임 목록 조회 추가 (#53)
    * remove: CRA로 생긴 파일 정리
    
    - public 폴더 내 파일 (logo, favicon, manifest.json)
    - 기본 CSS 파일 (index.css, App.css)
    - 성능 측정 도구 (reportWebVitals.js)
    - 테스트 파일 (App.test.js)
    
     #18
    
    * docs: pr 템플릿 내용 변경 (#30)
    
    pr 템플릿을 약간 수정하였습니다
    
    * feat(onboarding): 온보딩 화면에 들어갈 이미지와 아이콘 추가
    
    - 온보딩 화면에서 서비스의 기능 설명을 위한 사진을 추가하였습니다.
    - 카카오 로그인 버튼을 위한 아이콘을 추가하였습니다.
    
    #31
    
    * feat(onboarding): 온보딩 화면을 구현
    
    - 온보딩 과정에 사용할 이미지를 추가
    - 제목, 부제목, 카카오 로그인 버튼을 추가
    - 기능 설명을 위해 화살표를 통한 네비게이션 기능을 구현했습니다.
    
    #31
    
    * feat(router): 온보딩 화면을 위한 라우트 설정 추가
    
    - 온보딩 화면으로 이동할 수 있는 라우트를 추가했습니다.
    
    #31
    
    * chore(onboarding): 폰트 변경
    
    폰트를 Pretendard로 변경하였습니다.
    
    #31
    
    * feat(onboarding): 작은 화면에서 적용되는 반응형 디자인 추가
    
    - 768px 이하의 화면에서 이미지가 위로 올라가도록 @media 쿼리 추가
    - 화면이 작을 때 레이아웃이 수직으로 정렬되도록 수정하였습니다.
    
    #31
    
    * chore(gitignore): .vscode 부분을 git으로 관리하지 않도록 수정
    
    #36
    
    * design(reset.css): 기본적인 color와 text-decorator 제거
    
    #36
    
    * feat(meeting.mock): 모임 목록 조회 목서버 추가
    
    #36
    
    * feat(useGetMyMeeting): 모임 목록 조회 api 추가
    
    #36
    
    * feat(Spacing): Space 공통컴포넌트 추가
    
    * refactor(MeetingList): 불필요한 로직 제거
    
    #36
    
    ---------
    
    Co-authored-by: Da-Yeon-Kim <210941@jnu.ac.kr>
    Co-authored-by: Da-Yeon-Kim <168712207+Da-Yeon-Kim@users.noreply.github.com>
    Co-authored-by: HyeonChae1104 <211063@jnu.ac.kr>
    Co-authored-by: HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>

--- src/api/mock/meeting.mock.ts
+++ src/api/mock/meeting.mock.ts
@@ -1,8 +1,12 @@
 import { rest } from 'msw';
 
+import { getMyMeetingsPath } from '../hooks/useGetMyMeetings';
 import { createMeetingPath } from '../hooks/useCreateMeeting';
 
 export const meetingMockHandler = [
+  rest.get(getMyMeetingsPath(), (_, res, ctx) => {
+    return res(ctx.json(MY_MEETING_MOCK));
+  }),
   rest.post(createMeetingPath(), (_, res, ctx) => {
     return res(ctx.json(CREATE_MEETING_MOCK));
   }),
@@ -12,4 +16,39 @@ const CREATE_MEETING_MOCK = {
   status: 200,
   message: '모임 생성 성공',
   data: null,
+};
+
+const MY_MEETING_MOCK = {
+  status: null,
+  message: null,
+  data: [
+    {
+      meetingId: 1,
+      baseLocation: {
+        locationId: 18577297,
+        address: '경기 성남시 분당구 판교역로 166',
+        latitude: 37.39570088983171,
+        longitude: 127.1104335101161,
+      },
+      title: '밥먹자',
+      confirmedDateTime: '2022-10-28T03:00:00Z',
+      confirmedFood: {
+        food_id: 1001,
+        category: '한식',
+        name: '불고기',
+      },
+    },
+    {
+      meetingId: 2,
+      baseLocation: {
+        locationId: 18577297,
+        address: '경기 성남시 분당구 판교역로 166',
+        latitude: 37.39570088983171,
+        longitude: 127.1104335101161,
+      },
+      title: '밥먹자',
+      confirmedDateTime: null,
+      confirmedFood: null,
+    },
+  ],
 };
--- src/components/common/layouts/Container/index.tsx
+++ src/components/common/layouts/Container/index.tsx
@@ -6,13 +6,14 @@ import { vars } from '@/styles';
 type Props = {
   maxWidth?: string;
   flexDirection?: 'row' | 'column';
-  justifyContent?: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around';
+  justifyContent?: 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'left';
   alignItems?: 'center' | 'flex-start' | 'flex-end' | 'baseline' | 'stretch';
+  gap?: string;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 export const Container: React.FC<Props> = forwardRef(
   (
-    { children, maxWidth, flexDirection, justifyContent, alignItems, ...props }: Props,
+    { children, maxWidth, flexDirection, justifyContent, alignItems, gap, ...props }: Props,
     ref: React.Ref<HTMLDivElement>,
   ) => {
     return (
@@ -23,6 +24,7 @@ export const Container: React.FC<Props> = forwardRef(
           flexDirection={flexDirection}
           justifyContent={justifyContent}
           alignItems={alignItems}
+          gap={gap}
         >
           {children}
         </Inner>
@@ -39,12 +41,13 @@ const Wrapper = styled.div`
 `;
 
 const Inner = styled.div<
-  Pick<Props, 'maxWidth' | 'flexDirection' | 'justifyContent' | 'alignItems'>
+  Pick<Props, 'maxWidth' | 'flexDirection' | 'justifyContent' | 'alignItems' | 'gap'>
 >`
   width: 100%;
   max-width: ${({ maxWidth }) => maxWidth ?? vars.breakpoints.md};
   display: flex;
   flex-direction: ${({ flexDirection }) => flexDirection ?? 'column'};
-  justify-content: ${({ justifyContent }) => justifyContent ?? 'flex-start'};
+  justify-content: ${({ justifyContent }) => justifyContent ?? 'left'};
   align-items: ${({ alignItems }) => alignItems ?? 'flex-start'};
+  gap: ${({ gap }) => gap ?? '0'};
 `;
