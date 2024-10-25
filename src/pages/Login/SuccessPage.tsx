7dfe2dc0bccd4e400f2643bc8137987f345eae0d
HyeonChae1104 <211063@jnu.ac.kr>
2024-10-25 20:50:49 +0900

    refactor(SuccessPage): SuccessPage의 로직을 훅으로 분리
    - getCookieValue를 통해 accessToken을 쿠키에서 가져옵니다.
    -accessToken 확인 후 localStorage에 저장하고, 보안을 위해 그 값을 바로 삭제합니다.
    - accessToken이 존재하면 홈 페이지(/)로 이동하고, 존재하지 않으면 실패 페이지(/failure)로 이동합니다.
    
    #40

--- src/pages/Login/SuccessPage.tsx
+++ src/pages/Login/SuccessPage.tsx
@@ -1,31 +1,12 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useSuccessLogin } from 'src/api/hooks/useSuccessLogin';
 
-const SuccessPage: React.FC = () => {
-  const navigate = useNavigate();
+export const SuccessPage: React.FC = () => {
+  useSuccessLogin();
 
-  useEffect(() => {
-    const checkAccessToken = async () => {
-      try {
-        const accessToken = localStorage.getItem('accessToken');
-        if (!accessToken) throw new Error('토큰 없음');
-
-        await axios.get('/api/verify-token', {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-        navigate('/');
-      } catch (error) {
-        console.error('토큰 검증 실패:', error);
-        navigate('/failure'); // 유효하지 않으면 실패 페이지로 이동
-      }
-    };
-    checkAccessToken();
-  }, [navigate]);
-
-  return <div>토큰 검증 중...</div>;
+  return (
+    <div>
+      <h1>로그인 성공!</h1>
+    </div>
+  );
 };
-
-export default SuccessPage;
