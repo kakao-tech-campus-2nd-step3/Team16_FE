db3164e474c2a03688892788b9a733883bd40b3b
HyeonChae1104 <211063@jnu.ac.kr>
2024-10-25 21:28:32 +0900

    refactor(useSuccessLogin): 로그인 성공 시 쿠키 처리 로직 추가
    - 로그인 성공 후 토큰 처리 기능 구체화하였습니다.

--- src/api/hooks/useSuccessLogin.ts
+++ src/api/hooks/useSuccessLogin.ts
@@ -1,11 +1,20 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { deleteCookie, getCookieValue } from 'src/utils/cookie';
 
 export const useSuccessLogin = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
+    const getCookieValue = (name: string): string | undefined => {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop()?.split(';').shift();
+    };
+
+    const deleteCookie = (name: string) => {
+      document.cookie = `${name}=; Max-Age=0; path=/;`;
+    };
+
     const accessToken = getCookieValue('accessToken');
 
     if (accessToken) {
