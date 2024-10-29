52dd2330b715d8ac5be5e21f8206b9a7f27fb619
박건규 <geongyu09@gmail.com>
2024-10-29 16:50:33 +0900

    api 나오면 그때 컴포넌트 만들기

--- src/components/features/Group/GroupCalendar/index.tsx
+++ src/components/features/Group/GroupCalendar/index.tsx
@@ -1,31 +1,38 @@
+// import { useState } from 'react';
+
 import { PublicCalendar } from '@/service/Calendar';
+// import type { CalendarEvent } from '@/service/Calendar/components/PublicCalendar';
 
 export const GroupCalendar: React.FC = () => {
+  // const [selectedEvents, setSelectedEvents] = useState<CalendarEvent[]>([]);
+
+  // console.log(selectedEvents);
+
+  // const handleEventSelect = (newEvent: CalendarEvent) => {
+  //   setSelectedEvents((prev) => [...prev, newEvent]);
+  // };
+
   const events = [
     {
-      start_at: '2022-10-28T03:00:00Z',
-      end_at: '2022-10-29T06:00:00Z',
-      time_zone: 'Asia/Seoul',
-      all_day: false,
+      start: '2024-10-28T10:00:00',
+      end: '2024-10-28T12:00:00',
+      display: 'background', // 배경 이벤트로 설정
+      backgroundColor: 'rgba(59, 130, 246)', // 낮은 불투명도의 파란색
     },
     {
-      start_at: '2022-10-28T03:00:00Z',
-      end_at: '2022-10-29T06:00:00Z',
-      time_zone: 'Asia/Seoul',
-      all_day: false,
-    },
-    {
-      start_at: '2022-10-28T03:00:00Z',
-      end_at: '2022-10-29T06:00:00Z',
-      time_zone: 'Asia/Seoul',
-      all_day: false,
-    },
-    {
-      start_at: '2022-10-28T03:00:00Z',
-      end_at: '2022-10-29T06:00:00Z',
-      time_zone: 'Asia/Seoul',
-      all_day: false,
+      start: '2024-10-28T13:00:00',
+      end: '2024-10-28T18:00:00',
+      display: 'background',
+      backgroundColor: 'rgba(59, 130, 246)',
     },
   ];
-  return <PublicCalendar event={events} />;
+
+  return (
+    <PublicCalendar
+      events={events}
+      // availableStart="2024-10-28T00:00:00"
+      // availableEnd="2024-11-03T23:59:59"
+      // onEventSelect={handleEventSelect}
+    />
+  );
 };
