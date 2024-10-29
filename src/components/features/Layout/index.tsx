391cc0f687c6bc10bd74568f4c4c78693ce54536
박건규 <geongyu09@gmail.com>
2024-10-29 17:59:03 +0900

    feat(Footer): 푸터 구현
    #77

--- src/components/features/Layout/index.tsx
+++ src/components/features/Layout/index.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
 
+import { Spacing } from '@/components/common/layouts/Spacing';
+
+import { Footer } from './Footer';
 import { Header } from './Header';
 
 export const Layout = () => (
@@ -9,6 +12,8 @@ export const Layout = () => (
     <InnerWrapper>
       <Outlet />
     </InnerWrapper>
+    <Spacing height={80} />
+    <Footer />
   </Wrapper>
 );
 
@@ -17,8 +22,4 @@ const Wrapper = styled.div`
   position: relative;
 `;
 
-const InnerWrapper = styled.div`
-  width: 100%;
-  position: sticky;
-  top: 0;
-`;
+const InnerWrapper = styled.div``;
