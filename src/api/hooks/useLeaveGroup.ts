4c88055e2ee2a7fe64ad8e3575c96630155c52c6
박건규 <geongyu09@gmail.com>
2024-10-29 19:08:46 +0900

    feat(useLeaveGroup): 그룹 나가기 api 로직 추가
    #74

---
+++ src/api/hooks/useLeaveGroup.ts
@@ -0,0 +1,21 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { baseURL, fetchWithToken } from '../instance';
+
+interface LeaveGroupParams {
+  meetingId: string;
+}
+
+export const getLeaveGroupPath = ({ meetingId }: LeaveGroupParams) =>
+  `${baseURL}/meeting/${meetingId}`;
+
+export const deleteLeaveGroup = async ({ meetingId }: LeaveGroupParams) => {
+  const response = await fetchWithToken.delete(getLeaveGroupPath({ meetingId }));
+  return response;
+};
+
+export const useLeaveGroup = () => {
+  return useMutation({
+    mutationFn: deleteLeaveGroup,
+  });
+};
