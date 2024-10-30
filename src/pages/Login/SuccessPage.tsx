ceebb6ebffbbf3cfa0022d8567e7f48465366ac5
HyeonChae1104 <211063@jnu.ac.kr>
2024-10-31 00:41:56 +0900

    refactor(SuccessPage): 로그인 성공 시 redirect 되는 페이지 구현
    - API 요청으로 액세스 토큰 재발급 및 로컬 저장소에 저장
    - 토큰 재발급 후 홈 페이지로 자동 이동합니다.
    
    #40

--- src/pages/Login/SuccessPage.tsx
+++ src/pages/Login/SuccessPage.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { useSuccessLogin } from 'src/api/hooks/useSuccessLogin';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const SuccessPage: React.FC = () => {
-  useSuccessLogin();
+import { RouterPath } from '@/routes/path';
 
-  return (
-    <div>
-      <h1>로그인 성공!</h1>
-    </div>
-  );
-<<<<<<< HEAD
+export const SuccessPage = () => {
+  const navigate = useNavigate(); // useNavigate 훅을 사용하여 페이지를 이동합니다.
+
+  const reissueAccessToken = async () => {
+    try {
+      const baseURL = process.env.REACT_APP_API_URL
+      console.log('baseURL:', baseURL);
+      const response = await axios.get(`${baseURL}/reissue`, {
+        withCredentials: true, // 쿠키를 포함하여 요청
+      });
+
+      if (response.status === 200) {
+        console.log('토큰 재발급 성공:', response.headers);
+        const newAccessToken = response.headers.accessToken;
+        localStorage.setItem("accessToken", newAccessToken); 
+      } else {
+        // 실패 처리
+        console.error('Failed to reissue access token:', response.statusText);
+      }
+    } catch (error) {
+      console.error('토큰을 가져오던 중 에러 발생', error);
+    }
+  };
+
+  useEffect(() => {
+    // 페이지에 들어오면 reissueAccessToken 함수 실행
+    reissueAccessToken().then(() => {
+      // 토큰 재발급 후 필요한 작업 수행 (페이지 이동)
+      navigate(RouterPath.home);
+    });
+  }, [navigate]);
+
+  return null;
 };
-=======
-};
->>>>>>> 1083c82 (feat(routes): 성공 및 실패 페이지 경로 추가)
