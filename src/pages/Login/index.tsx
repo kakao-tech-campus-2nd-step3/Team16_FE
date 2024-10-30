a92784a064e665d2675cf8c27a519563db9e66c4
HyeonChae1104 <211063@jnu.ac.kr>
2024-10-31 00:52:22 +0900

    refactor(LoginPage): 로그인 페이지 기능 수정
    - 로그인 버튼을 통해 백엔드 로그인로직으로 redirect 합니다.
    
    #40

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { useHandleKakaoLogin } from 'src/api/hooks/useHandleKakaoLogin';
 import { KakaoLoginButton } from 'src/components/common/Button/kakaoLogin';
 
 export const LoginPage: React.FC = () => {
-  const handleKakaoLogin = useHandleKakaoLogin();
+  const login = () => {
+    const baseURL = process.env.REACT_APP_API_URL
+    window.location.href = `${baseURL}/login`
+  }
 
   return (
     <div>
       <h1>로그인 페이지</h1>
-      <KakaoLoginButton onClick={() => handleKakaoLogin()} />
+      <KakaoLoginButton onClick={login} />
     </div>
   );
 };
-
-export default LoginPage;
