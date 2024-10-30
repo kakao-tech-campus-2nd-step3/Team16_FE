a383ca1ce67843a81f7dd4e7cf1c8beb4957092b
HyeonChae1104 <211063@jnu.ac.kr>
2024-10-31 00:48:49 +0900

    refactor(FailurePage): 로그인 실패 시 redirect 되는 페이지 구현
    - 로그인 실패 시 에러 문구
    - 로그인 버튼을 누르면 다시 백엔드 로그인 로직으로 redirect 되도록 설정하였습니다.
    
    #40

--- src/pages/Login/FailurePage.tsx
+++ src/pages/Login/FailurePage.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { useHandleKakaoLogin } from 'src/api/hooks/useHandleKakaoLogin';
 import { KakaoLoginButton } from 'src/components/common/Button/kakaoLogin';
 
 export const FailurePage: React.FC = () => {
-  const handleKakaoLogin = useHandleKakaoLogin();
+  const login = () => {
+    const baseURL = process.env.REACT_APP_API_URL
+    window.location.href = `${baseURL}/login`
+  }
 
   return (
     <div>
       <h1>로그인 실패</h1>
       <p>카카오 서버에 문제가 있거나, 로그인이 취소되었습니다. 다시 시도해 주세요.</p>
-      <KakaoLoginButton onClick={() => handleKakaoLogin(true)} />
+      <KakaoLoginButton onClick={login} />
     </div>
   );
 };
