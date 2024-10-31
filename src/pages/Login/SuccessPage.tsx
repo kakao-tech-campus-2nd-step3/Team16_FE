df103f52f5f0ba96b59b8fb1ac7acf3e2c4b1575
HyeonChae1104 <211063@jnu.ac.kr>
2024-11-01 02:35:31 +0900

    feat(SuccessPage): SuccessPage의 API 호출 로직 분리 및 fetchWithToken 설정 반영
    - SuccessPage에서 API 호출 로직을 분리
    - fetchWithToken 인스턴스를 사용하여 Authorization 헤더 및 baseURL 설정을 적용하였습니다.
    
    #40

--- src/pages/Login/SuccessPage.tsx
+++ src/pages/Login/SuccessPage.tsx
@@ -1,40 +1,24 @@
-import axios from 'axios';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { useReissueAccessToken } from '@/api/hooks/useReissueAccessToken';
 import { RouterPath } from '@/routes/path';
 
 export const SuccessPage = () => {
-  const navigate = useNavigate(); // useNavigate 훅을 사용하여 페이지를 이동합니다.
-
-  const reissueAccessToken = async () => {
-    try {
-      const baseURL = process.env.REACT_APP_API_URL
-      console.log('baseURL:', baseURL);
-      const response = await axios.get(`${baseURL}/reissue`, {
-        withCredentials: true, // 쿠키를 포함하여 요청
-      });
-
-      if (response.status === 200) {
-        console.log('토큰 재발급 성공:', response.headers);
-        const newAccessToken = response.headers.accessToken;
-        localStorage.setItem("accessToken", newAccessToken); 
-      } else {
-        // 실패 처리
-        console.error('Failed to reissue access token:', response.statusText);
-      }
-    } catch (error) {
-      console.error('토큰을 가져오던 중 에러 발생', error);
-    }
-  };
+  const navigate = useNavigate();
+  const { refetch, status, error } = useReissueAccessToken();
 
   useEffect(() => {
-    // 페이지에 들어오면 reissueAccessToken 함수 실행
-    reissueAccessToken().then(() => {
-      // 토큰 재발급 후 필요한 작업 수행 (페이지 이동)
-      navigate(RouterPath.home);
-    });
-  }, [navigate]);
+    refetch(); // 컴포넌트가 로드될 때 토큰 재발급 요청 수행
+  }, [refetch]);
+
+  useEffect(() => {
+    if (status === 'success') {
+      navigate(RouterPath.home); // 성공 시 홈 페이지로 이동
+    } else if (status === 'error' && error) {
+      console.error('토큰을 가져오던 중 에러 발생', error);
+    }
+  }, [status, error, navigate]);
 
   return null;
 };
