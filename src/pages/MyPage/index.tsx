0462b729cb0f7e8bd08c45fb0f5a65687b010ea7
HyeonChae1104 <211063@jnu.ac.kr>
2024-11-08 17:18:39 +0900

    refactor(MyPage): presentation-container 패턴 적용 수정
    - useGetUserProfile 사용하여 사용자 데이터 가져옵니다.
    - UserProfileImage의 로직 사용
    - ProfileSection 컴포넌트에서 데이터 관련 부분을 관리하고 `MyPage/index.tsx`에서는 해당 섹션을 불러와 렌더링합니다.
    
    #64

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,40 +1,12 @@
-import styled from '@emotion/styled';
 import React from 'react';
+import { ProfileSection }from 'src/components/features/MyPage/ProfileSection';
 
-import { UserProfileImage } from '@/components/common/User/UserProfileImage';
-
-interface ProfileData {
-  profile_image_url: string;
-  nickname: string;
-}
-
-interface MyPageProps {
-  profileData?: ProfileData;
-}
-
-export const MyPage: React.FC<MyPageProps> = ({ profileData }) => (
-  <ProfileContainer>
-    <UserProfileImage profileImageUrl={profileData?.profile_image_url || '/images/default_profile_image.png'} size="lg" />
-    <ProfileName>{profileData?.nickname || '쿠키즈'}</ProfileName>
-  </ProfileContainer>
-);
+export const MyPage: React.FC = () => {
+  return (
+    <div>
+      <ProfileSection />
+    </div>
+  );
+};
 
 export default MyPage;
-
-// 스타일링 컴포넌트
-const ProfileContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  height: calc(100vh - 4rem);
-  justify-content: flex-start;
-  padding-top: 2rem;
-  text-align: center;
-`;
-
-const ProfileName = styled.h2`
-  font-size: 1.6rem;
-  font-weight: 600;
-  color: #333;
-  margin-top: 0.8rem;
-`;
