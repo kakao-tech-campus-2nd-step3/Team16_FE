74daa79bc7a4c4e3dadaf2eedd623d18fb6c9cec
HyeonChae1104 <129866491+HyeonChae1104@users.noreply.github.com>
2024-11-08 19:30:14 +0900

    [FEAT] MyPage 프로필 조회 구현 (#96)
    * feat(Header): 헤더에 프로필 아이콘 클릭 시 마이페이지로 이동 기능 추가
    
    - handleUserIconClick 함수 추가하여 HeaderUserIcon에 onClick으로 전달
    로고 클릭 시 메인 페이지로 이동하는 기능
    HeaderUserIcon에 onClick 전달
    
    #64
    
    * feat(MyPage):  마이페이지 UI 및 프로필 조회 기능 추가
    
    - - 백엔드에서 사용자 프로필 정보를 받아와 화면에 표시
    - 프로필 이미지와 닉네임을 중앙 상단에 배치
    - mypage에서 사용자 프로필 이미지를 큰 사이즈로 표시하기 위해 UserProfileImage 컴포넌트에  "lg" 사이즈 옵션을 추가
    
    #64
    
    * refactor(HeaderUserIcon): UI와 클릭로직의 책임 분리
    
    - HeaderUserIcon은 UI를 담당,
    - Header에서 클릭 시 마이페이지로 이동하는 것을 수행하도록 분리하였습니다.
    
    #64
    
    * refactor(MyPage): presentation-container 패턴 적용
    
    - useGetUserProfile 훅을 사용해 데이터를 가져오고, MyPage는 props로 전달받아 렌더링만 담당
    - MyPage 컴포넌트를 상위 MyPageContainer에서 데이터 로직을 처리하도록 리팩토링하였습니다.
    
    #64
    
    * refactor(HeadUserIcon): HeaderUserIcon 컴포넌트 재사용성 개선
    
    - HeaderUserIcon에서 navigate 기능을 제거하고, 프로필 이미지를 렌더링하는 역할에 집중,
    - Header 컴포넌트에서 RouterPath 객체 사용
    
    #64
    
    * refactor(HeaderUserIcon): onClick 제거
    
    - onClick 제거
    - Header에서 Link태그를 사용하도록 변경하였습니다.
    
    #64
    
    * refactor(MyPage): page 컴포넌트를 부르도록 routes 수정
    
    - Container에서 Section으로 컴포넌트 이름 변경
    - page 컴포넌트가 최상위가 되도록 수정하였습니다.
    
    #64
    
    * refactor(MyPage): presentation-container 패턴 적용 수정
    
    - useGetUserProfile 사용하여 사용자 데이터 가져옵니다.
    - UserProfileImage의 로직 사용
    - ProfileSection 컴포넌트에서 데이터 관련 부분을 관리하고 `MyPage/index.tsx`에서는 해당 섹션을 불러와 렌더링합니다.
    
    #64
    
    * refactor(MyPage): 절대 경로로 import 경로 수정
    
    #64

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -1,3 +1,14 @@
-export const MyPage = () => {
-  return <div>MyPage</div>;
+import React from 'react';
+
+import { ProfileSection } from '@/components/features/MyPage/ProfileSection';
+
+
+export const MyPage: React.FC = () => {
+  return (
+    <div>
+      <ProfileSection />
+    </div>
+  );
 };
+
+export default MyPage;
