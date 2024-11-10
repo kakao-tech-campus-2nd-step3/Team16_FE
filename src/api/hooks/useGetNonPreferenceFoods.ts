b185bdaea56d5dc86eac3d4b8d91f7c8c228536e
HyeonChae1104 <211063@jnu.ac.kr>
2024-11-10 10:11:05 +0900

    feat(hooks): 선호/비선호 음식 조회 구현
    - 사용자 선호 음식을 조회하는 `useGetPreferenceFoods` 훅 추가
    - 사용자 비선호 음식을 조회하는 `useGetNonPreferenceFoods` 훅 추가
    
    #95

---
+++ src/api/hooks/useGetNonPreferenceFoods.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { baseURL, fetchWithToken } from '@/api/instance';
+
+const getNonPreferencePath = () => `${baseURL}/non-preferences`; 
+
+export const getNonPreferenceFoods = async () => {
+  const response = await fetchWithToken.get(getNonPreferencePath()); 
+  return response.data.data || [];
+};
+
+export const useGetNonPreferenceFoods = () => {
+  return useQuery({
+    queryKey: ['nonPreferenceFoods'],
+    queryFn: getNonPreferenceFoods,
+    staleTime: 1000 * 60 * 60 * 24, 
+  });
+};
---
+++ src/api/hooks/useGetPreferenceFoods.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { baseURL, fetchWithToken } from '@/api/instance';
+
+const getPreferencePath = () => `${baseURL}/preferences`;
+
+export const getPreferenceFoods = async () => {
+  const response = await fetchWithToken.get(getPreferencePath());
+  return response.data.data || [];
+};
+
+export const useGetPreferenceFoods = () => {
+  return useQuery({
+    queryKey: ['preferenceFoods'],
+    queryFn: getPreferenceFoods,
+    staleTime: 1000 * 60 * 60 * 24,
+  });
+};
