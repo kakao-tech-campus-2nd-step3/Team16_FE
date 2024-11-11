fc53b45e9ab29d77a84fc487054f757d21ae7db3
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-11-11 13:34:57 +0900

    feat(Login): 불필요한 목 핸들러 제거
     #114

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,13 +1,11 @@
 import { setupWorker } from 'msw';
 
 import { calendarMockHandler } from '@/api/mock/calendar.mock';
-import { LoginMockhandler } from '@/api/mock/login.mock';
 import { meetingMockHandler } from '@/api/mock/meeting.mock';
 import { userMockhandler } from '@/api/mock/user.mock';
 
 export const worker = setupWorker(
   ...userMockhandler,
-  ...LoginMockhandler,
   ...meetingMockHandler,
   ...calendarMockHandler,
 );
