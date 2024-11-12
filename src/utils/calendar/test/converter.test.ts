94b57882dc0e82508c3338b202d57ade14320360
박건규 <geongyu09@gmail.com>
2024-11-13 00:11:08 +0900

    test: defaultEventToGroupEvent 테스트 추가

--- src/utils/calendar/test/converter.test.ts
+++ src/utils/calendar/test/converter.test.ts
@@ -104,4 +104,38 @@ describe('converter', () => {
       expect(result).toHaveLength(2);
     });
   });
+  describe('defaultEventToGroupEvent', () => {
+    it('defaultEvent 타입의 객처 배열을 GroupEvent타입의 객체 배열로 변경한다.', () => {
+      const events = [
+        {
+          start: new Date('2023-01-01T10:00:00Z').toString(),
+          end: new Date('2023-01-01T12:00:00Z').toString(),
+        },
+        {
+          start: new Date('2023-01-02T10:00:00Z').toString(),
+          end: new Date('2023-01-02T12:00:00Z').toString(),
+        },
+      ];
+
+      const expected = [
+        {
+          start: new Date('2023-01-01T10:00:00Z').toString(),
+          end: new Date('2023-01-01T12:00:00Z').toString(),
+          backgroundColor: 'rgba(105, 132, 116, 0.7)',
+          borderColor: '698474',
+          display: 'background',
+        },
+        {
+          start: new Date('2023-01-02T10:00:00Z').toString(),
+          end: new Date('2023-01-02T12:00:00Z').toString(),
+          backgroundColor: 'rgba(105, 132, 116, 0.7)',
+          borderColor: '698474',
+          display: 'background',
+        },
+      ];
+
+      const result = defaultEventToGroupEvent(events);
+      expect(result).toEqual(expected);
+    });
+  });
 });
