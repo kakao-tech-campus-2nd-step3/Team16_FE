90f33e29b1b9887d09f865b09a12ca6569a7f28e
박건규 <geongyu09@gmail.com>
2024-11-12 23:23:05 +0900

    test: sortTimes 함수 테스트 추가
    #127

---
+++ src/utils/calendar/test/calculator.test.ts
@@ -0,0 +1,87 @@
+import type { SelectedTime } from '@/types';
+
+import { sortTimes } from '../calculator';
+
+describe('calculator', () => {
+  describe('sortTimes', () => {
+    it('시작 시간을 기준으로 오름차순으로 정렬한다.', () => {
+      const times = [
+        {
+          startAt: '2023-12-02T10:00:00',
+          endAt: '2023-12-02T11:00:00',
+          allDay: false,
+          timeZone: 'Asia/Seoul',
+        },
+        {
+          startAt: '2023-12-02T09:00:00',
+          endAt: '2023-12-02T10:00:00',
+          allDay: false,
+          timeZone: 'Asia/Seoul',
+        },
+        {
+          startAt: '2023-12-02T11:00:00',
+          endAt: '2023-12-02T12:00:00',
+          allDay: false,
+          timeZone: 'Asia/Seoul',
+        },
+      ];
+
+      const result = sortTimes(times);
+
+      expect(result).toEqual([
+        {
+          allDay: false,
+          endAt: '2023-12-02T10:00:00',
+          startAt: '2023-12-02T09:00:00',
+          timeZone: 'Asia/Seoul',
+        },
+        {
+          allDay: false,
+          endAt: '2023-12-02T11:00:00',
+          startAt: '2023-12-02T10:00:00',
+          timeZone: 'Asia/Seoul',
+        },
+        {
+          allDay: false,
+          endAt: '2023-12-02T12:00:00',
+          startAt: '2023-12-02T11:00:00',
+          timeZone: 'Asia/Seoul',
+        },
+      ]);
+    });
+
+    it('빈배열을 전달받으면 빈 배열을 반환한다.', () => {
+      const times: SelectedTime[] = [];
+      const result = sortTimes(times);
+      expect(result).toEqual([]);
+    });
+
+    it('원본 배열을 수정하지 않는다.', () => {
+      const times = [
+        {
+          startAt: '2023-12-02T10:00:00',
+          endAt: '2023-12-02T11:00:00',
+          allDay: false,
+          timeZone: 'Asia/Seoul',
+        },
+        {
+          startAt: '2023-12-02T09:00:00',
+          endAt: '2023-12-02T10:00:00',
+          allDay: false,
+          timeZone: 'Asia/Seoul',
+        },
+        {
+          startAt: '2023-12-02T11:00:00',
+          endAt: '2023-12-02T12:00:00',
+          allDay: false,
+          timeZone: 'Asia/Seoul',
+        },
+      ];
+      const original = [...times];
+
+      sortTimes(times);
+
+      expect(times).toEqual(original);
+    });
+  });
+});
