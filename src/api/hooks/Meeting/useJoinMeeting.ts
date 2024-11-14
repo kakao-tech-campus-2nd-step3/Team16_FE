59dcef194803d17e8ca0756162fd71b3746200af
Da-Yeon-Kim <210941@jnu.ac.kr>
2024-11-15 02:58:55 +0900

    fix: joinData에 안 담도록 수정
     #177

--- src/api/hooks/Meeting/useJoinMeeting.ts
+++ src/api/hooks/Meeting/useJoinMeeting.ts
@@ -3,29 +3,24 @@ import { useMutation } from '@tanstack/react-query';
 import { baseURL, fetchWithToken } from '@/api/instance';
 import type { JoinMeetingRequest, JoinMeetingResponse } from '@/types';
 
+interface JoinParams {
+  meetingId: string;
+  joinData: JoinMeetingRequest;
+}
+
 export const joinMeetingPath = ({ meetingId }: { meetingId: string }) =>
   `${baseURL}/meeting/${meetingId}/join`;
 
 export const joinMeeting = async ({
   meetingId,
   joinData,
-}: {
-  meetingId: string;
-  joinData: JoinMeetingRequest;
-}) => {
-  const response = await fetchWithToken<JoinMeetingResponse>(joinMeetingPath({ meetingId }), {
-    method: 'POST',
-    data: joinData,
-  });
+}: JoinParams): Promise<JoinMeetingResponse> => {
+  const response = await fetchWithToken.post(joinMeetingPath({ meetingId }), joinData);
   return response.data;
 };
 
 export const useJoinMeeting = () => {
-  return useMutation<
-    JoinMeetingResponse,
-    Error,
-    { meetingId: string; joinData: JoinMeetingRequest }
-  >({
+  return useMutation<JoinMeetingResponse, Error, JoinParams>({
     mutationFn: ({ meetingId, joinData }) => joinMeeting({ meetingId, joinData }),
   });
 };
