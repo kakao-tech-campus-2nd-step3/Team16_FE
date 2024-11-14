43457ddb0dec8b409443f6748e1531c4ab690a01
HyeonChae1104 <211063@jnu.ac.kr>
2024-11-14 10:23:35 +0900

    feat(FailurePage): 텍스트와 버튼이 화면 중앙에 위치하도록 수정
    - 로그인 오류 멘트 수정
    
    #147

--- src/pages/Login/FailurePage.tsx
+++ src/pages/Login/FailurePage.tsx
@@ -9,10 +9,21 @@ export const FailurePage: React.FC = () => {
   };
 
   return (
-    <div>
+    <div style={styles.container}>
       <h1>로그인 실패</h1>
-      <p>카카오 서버에 문제가 있거나, 로그인이 취소되었습니다. 다시 시도해 주세요.</p>
+      <p>로그인 중 오류가 발생하였습니다. 다시 시도해 주세요.</p>
       <KakaoLoginButton onClick={login} />
     </div>
   );
+};
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column' as const,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    textAlign: 'center' as const,
+  },
 };
