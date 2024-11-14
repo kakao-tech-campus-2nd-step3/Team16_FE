00ef2af51c7b74dc73b075ce019130cef34b3c51
박건규 <geongyu09@gmail.com>
2024-11-15 01:21:29 +0900

    fix: 범위 축소
    #162

--- src/utils/calculator/index.ts
+++ src/utils/calculator/index.ts
@@ -2,14 +2,14 @@ export const getCurrentDateStrings = (baseDate: string) => {
   const date = new Date(baseDate);
 
   const plus15Days = new Date(date.getTime() + 15 * 24 * 60 * 60 * 1000);
-  const minus14Days = new Date(date.getTime() - 14 * 24 * 60 * 60 * 1000);
+  const minus13Days = new Date(date.getTime() - 13 * 24 * 60 * 60 * 1000);
 
   const formatDate = (d: Date): string => {
     return d.toISOString().slice(0, 19) + 'Z';
   };
 
   return {
-    from: formatDate(minus14Days),
+    from: formatDate(minus13Days),
     to: formatDate(plus15Days),
   };
 };
